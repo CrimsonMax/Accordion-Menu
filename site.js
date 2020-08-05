2bbed75241415b46850b0bee40adaba812321e91
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2020-08-06 00:48:09 +0300

    Done!

--- site.js
+++ site.js
@@ -1,7 +1,8 @@
-function showText() {
-    const text = document.querySelector('.hidderRidds');
-    text.classList.remove('hidden');
+let clickedElem = document.querySelectorAll('.plus');
+
+for (i = 0; i < clickedElem.length; i++) {
+    let aim = clickedElem[i].previousElementSibling.lastElementChild;
+    clickedElem[i].addEventListener('click', function () {
+        aim.classList.toggle("hidden");
+    });
 }
-
-
-document.querySelector('.plus').addEventListener('click', showText);
